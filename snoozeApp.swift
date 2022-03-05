a97254ee9fffba3f73a18e3b5691582e5fdf076a
dereq <dereq@Ontario.local>
2022-03-05 17:45:47 -0500

    Project setup

---
+++ snoozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  snoozeApp.swift
+//  Shared
+//
+//  Created by dereq on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct snoozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
